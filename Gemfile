67937ccecea14d7592e57202115cad7010eb49e7
Christophe Augello <christophe@augello.be>
2014-03-16 18:38:55 +0100

    Refactoring + use T411 gem

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://www.rubygems.org'
+
+gem 'thor'
+gem 'json'
+gem 'sanitize'
+gem 'colored'
+gem 't411'
--- gratt.rb
+++ gratt.rb
@@ -1,46 +1,27 @@
 #!/usr/bin/ruby
-require 'rest_client'
 require 'json'
 require 'thor'
 require 'sanitize'
 require 'colored'
+require 't411'
 
 class Gratt < Thor
 $server = 'Server IP'
-username = 'Your login'
-password = 'Your Pwd'
-$t411_api = 'https://api.t411.me'
-login = RestClient.post "#{$t411_api}/auth", { 'username' => "#{username}", 'password' => "#{password}" }, :content_type => :json, :accept => :json
-$auth = JSON.parse(login)
+T411.authenticate('username','password')
 
-desc 'top', 'Periods today,week,month,top100  && cat: film, documentaire, serie, audiobook, animation'
-def top(period,cat)
-  begin
-  periods = {today: 'today', week: 'week', month: 'month', top100: 'top100' }
-  category = {film: 631, documentaire: 634, serie: 433, audiobook: 405, animation: 455}
-  request = RestClient.get "#{$t411_api}/torrents/top/#{periods[:"#{period}"]}", headers = {Authorization: "#{$auth['token']}" }
-  today = JSON.parse(request)
-  today.each do |item|
-    if item['category'].to_i == category[:"#{cat}"]
+desc 'top', 'Periods today,month,top100'
+def top(period)
+  result = JSON.parse(T411::Torrents.send(period.to_sym))
+  result.each do |item|
     puts item['id'] + ' ' + item['name'] + ' ' + item['category']
-    end
-  end
-  rescue
-   puts 'Error !!'
   end
 end
 
 desc 'search TITLE LIMIT','search on title and limit the amount of result'
-def search(title,limit = 50)
-  title = title.dup
-  while title.include?(' ') do
-  title.sub!(/ /, '+')
-  end
+def search(title,limit = 100)
   begin
-  request = RestClient.get "#{$t411_api}/torrents/search/#{title}&offset=10&limit=#{limit}", headers = {Authorization: "#{$auth['token']}" }
-  search = JSON.parse(request)
-  search['torrents'].each do |x|
-  puts " #{x['id']}  #{x['name']} #{(x['size'].to_i/1000000)} Mb"
+    JSON.parse(T411::Torrents.search(title,limit))['torrents'].each do |x|
+    puts " #{x['id']}  #{x['name']} #{(x['size'].to_i/1000000)} Mb"
   end
   rescue
     puts 'No results found !!'
@@ -50,10 +31,7 @@ end
 desc 'download ID', 'download a specific ID'
 def download(id)
   begin
-  request = RestClient.get "#{$t411_api}/torrents/download/#{id}", headers = {Authorization: "#{$auth['token']}" }
-  File.open("#{id}.torrent", "a+") do |line|
-    line.puts request
-  end
+  T411::Torrents.download(id)
   system("transmission-remote #{$server} -a #{id}.torrent")
   system("rm #{id}.torrent ")
   rescue
@@ -64,12 +42,12 @@ end
 desc 'details ID', 'details of an ID'
 def details(id)
   begin
-  request = RestClient.get "#{$t411_api}/torrents/details/#{id}", headers = {Authorization: "#{$auth['token']}" }
-  details = JSON.parse(request)
-  puts Sanitize.clean(details['description']).yellow_on_black
+    details = JSON.parse(T411::Torrents.details(id))
+    puts Sanitize.clean(details['description']).yellow_on_black
   rescue
-  puts 'ID not found !!'
+    puts 'ID Not found'
   end
+
 end
 
 end
